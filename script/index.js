e3c6716492ee7afa7c155472342ebcdf766d7681
Mariia Tyshkovets <maria15021996@gmail.com>
2022-04-19 22:50:20 +0300

    little change for compare

--- script/index.js
+++ script/index.js
@@ -165,7 +165,7 @@ class EmpTable {
                 let td = document.createElement('TD');
                 td.innerHTML = employees[i][property];
                 tr.appendChild(td);
-              }
+            }
         }
     }
 }
